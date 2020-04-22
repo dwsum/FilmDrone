b779b38fd6816377077ba150d86e03ae4de30d42
dwsum <50306457+dwsum@users.noreply.github.com>
2020-04-22 13:57:50 -0600

    Add files via upload

---
+++ FilmDrone/cmake-build-debug/CMakeFiles/FilmDrone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FilmDrone.dir/main.cpp.o"
+  "FilmDrone.pdb"
+  "FilmDrone"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FilmDrone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FilmDrone/cmake-build-debug/CMakeFiles/FilmDrone.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FilmDrone.
+# This may be replaced when dependencies are built.
